2efb33c1166e9356521770c5e692678be3ae4031
mortis133 <kryst.madry@gmail.com>
2023-10-14 16:03:17 +0200

    Add clicker

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
-import React, { lazy, Suspense } from 'react';
+import React, { lazy, Suspense, useState } from 'react';
 import './App.css';
 const Header = lazy(() => import('ExampleRemote/Header'));
 const Footer = lazy(() => import('ExampleRemote/Footer'));
+const Clicker = lazy(() => import('ExampleRemote/Clicker'));
 
 function App() {
+  const [hostState, setHostState] = useState('initial');
   return (
     <div className="App">
       <Suspense fallback={<div>Ładowanie Headera...</div>}>
         <Header />
       </Suspense>
       <p>
-        Host App
+        Host App - {hostState}
       </p>
+      <Suspense fallback={<div>Ładowanie Clicker...</div>}>
+        <Clicker hostState={hostState} setHostState={setHostState} />
+      </Suspense>
       <Suspense fallback={<div>Ładowanie Footera...</div>}>
         <Footer />
       </Suspense>
